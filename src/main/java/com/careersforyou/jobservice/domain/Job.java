fa1f2da2222d65cf1b8d71738408a97590546aa1
pgarrett <iampaulgarrett@gmail.com>
2024-04-10 13:31:03 -0700

    Updated 4-10-2024

---
+++ src/main/java/com/careersforyou/jobservice/domain/Job.java
@@ -0,0 +1,23 @@
+package com.careersforyou.jobservice.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record Job (
+        @NotBlank(message = "The jobid must be defined.")
+        @Pattern(
+            regexp = "^([0-9]?\\d$)$",
+            message = "The jobid format must be valid."
+        )
+        String jobid,
+        @NotBlank(message = "The job title must be defined.")
+        String title,
+        @NotBlank(message = "The job description must be defined.")
+        String description,
+        @NotBlank(message = "The company name must be defined.")
+        String companyname,
+        @NotBlank(message = "Skill 1 must be defined")
+        String skill1,
+        @NotBlank(message = "Skill 2 must be defined")
+        String skill2
+){}
---
+++ src/main/java/com/careersforyou/jobservice/domain/JobAlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.careersforyou.jobservice.domain;
+public class JobAlreadyExistsException extends RuntimeException {
+    public JobAlreadyExistsException(String jobid) {
+        super("A job with Jobid " + jobid + " already exists.");
+    }
+}
